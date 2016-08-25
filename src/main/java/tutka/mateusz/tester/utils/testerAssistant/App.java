8dc3544025cd832739b4327d5ad45d211102780b
Mateusz Tutka <mateusz.tutka@gmail.com>
2016-08-25 21:00:22 +0200

    Establishing new project

---
+++ src/main/java/tutka/mateusz/tester/utils/testerAssistant/App.java
@@ -0,0 +1,13 @@
+package tutka.mateusz.tester.utils.testerAssistant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
